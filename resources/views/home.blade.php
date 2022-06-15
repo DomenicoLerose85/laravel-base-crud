6c4a9ba0db28ec6d1c9a664f427e598ec881bb4c
DomenicoLerose85 <domenicolerose85@libero.it>
2022-06-15 16:21:48 +0200

    layouts_partials_done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+
+@section('pageTitle', 'Home')
+
+@section('mainContent')
+    <h1 class="text-center">Ciao <i class="fa-solid fa-code-commit"></i></h1>
+    
+@endsection
