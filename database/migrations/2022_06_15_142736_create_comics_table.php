993b3dc861e91f4af0735c8d75d3405283273287
DomenicoLerose85 <domenicolerose85@libero.it>
2022-06-15 17:45:59 +0200

    migration_launched

--- database/migrations/2022_06_15_142736_create_comics_table.php
+++ database/migrations/2022_06_15_142736_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description')->nullable();
+            $table->string('image');
+            $table->float('price', 2, 2);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
